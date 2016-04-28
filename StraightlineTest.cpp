5e1d27d64e3c6ab1b86f39f22b27c3b270b5cba3
Lenton <lentonneale@gmail.com>
2016-04-28 03:37:02 +0000

    Fixed code again to work with updated library

--- StraightlineTest.cpp
+++ StraightlineTest.cpp
@@ -1,21 +1,26 @@
-# include <stdio.h> 
-# include <time.h> 
+# include <stdio.h>
+# include <time.h>
 
+//these load specific methods from the ENGR101 library
+extern "C" int init(int d_lev);
+extern "C" int Sleep( int sec , int usec );
+extern "C" int set_motor(int motor , int speed );
 
-// these load specific methods from the ENGR101 library 
-extern "C" int init_hardware();
-extern "C" int Sleep ( int sec , int usec );
-extern "C" int set_motor ( int motor , int dir , int speed );
+int main (){
 
-int main(){ 
-	// This sets up the RPi hardware and ensures 
-	// everything is working correctly 
-	init_hardware();
+   // This sets up the RPi hardware and ensures
+   // everything is working correctly
+   init(1);
 
-while (1) {
-	set_motor (1 , 1, 255);
+   // Sets the motor connected to pin 1 to rotate
+   // in one direction at MAX speed .
+   set_motor(1, 255); // pin 1 = right hand motor (red and purple wires)
 
-	set_motor (2 , 2, 255);
-}
+   // Sets the motor connected to pin 1 to rotate
+   // in the opposite direction at 50 % speed .
+   set_motor(2, 255); // pin 2 = left hand motor (green and yellow wires)
+   Sleep(1 ,000000);
 
+   set_motor(1,0);
+   set_motor(2,0);
 return 0;}
