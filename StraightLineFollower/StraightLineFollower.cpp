9f55a8a2358b55ba2a6bea02c0f1ad759fcc302d
Michael <wintonmj@me.com>
2016-04-29 05:42:55 +0000

    New method devised for error checking distance to line

--- StraightLineFollower/StraightLineFollower.cpp
+++ StraightLineFollower/StraightLineFollower.cpp
@@ -5,11 +5,10 @@
 extern "C" int init(int d_lev);
 extern "C" int take_picture();
 extern "C" char get_pixel(int row,int col,int colour);
-extern "C" char set_pixel(int row, int col, char red, char green, char blue);
-extern "C" int open_screen_stream();
-extern "C" int update_screen();
 extern "C" int Sleep(int sec, int usec);
 extern "C" int set_motor(int motor , int speed );
+
+int DEBUG = 1;
 
 int main() {
 	init(0);
@@ -18,54 +17,50 @@ int main() {
 	int ntp = 16;
 	double dx = width / ntp;
 	int whiteness[ntp];
-	int lineStart;
+	int zeroCentre[] = {-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8};
+    int n_whites = 0;
+    double error = 0.0; // Define later
+
+/*	int lineStart;
 	int lineEnd;
     double lineCentre;
     double arrayCentre = (ntp - 1)/2;
-    int k; // Define later
     double v_left = 128 + (k*(lineCentre-arrayCentre));
 	double v_right = 128 - (k*(lineCentre-arrayCentre));
-
-	open_screen_stream();
+*/
 
 	while (1) {
 
 		take_picture();
+		n_whites = 0;
 		for (int i = 0; i < 16; i++) {
 			int x = dx * i + (dx/2);
 			whiteness[i] = get_pixel(x,120,3);
-
 	                if (whiteness[i] > 100) {
                         // 1 = white
 	                    whiteness[i] = 1;
+	                    n_whites++;
 	                }
 	                else {
                         // 0 = black
                         whiteness[i] = 0;
 	                }
-
-	                set_pixel(x,120 + 2,255,0,0);
-          	        set_pixel(x,120 - 2,255,0,0);
-
+        printf("%d ", whiteness[i]);
 		}
+		printf("");
 
-        for (int k = 0; k < 15; k++) {
-            if (whiteness[k] == 0 && whiteness[k+1] == 1) {
-                lineStart = k+1;
-            }
-
-            if (whiteness[k] == 1 && whiteness[k+1] == 0) {
-                lineEnd = k;
-            }
-            lineCentre = (lineStart + lineEnd)/2;
+        error = 0.0;
+        for (int l = 0; l < 16; l++) {
+            error = error + zeroCentre[l] * whiteness[l];
         }
-
-		for (int j = 0; j < 16; j++) {
-            printf("%d ", whiteness[j]);
-		}
-
-		Sleep(0,100000);
-		printf("\n");
-		update_screen();
+        if (n_whites!=0)
+        {
+            error = error/((double)n_whites);
+            Sleep(0,100000);
+            printf("\n");
+        }
+        else {
+            printf("No line detected\n");
+        }
 	}
 }
